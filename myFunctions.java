67a046bdb4d6d20dcc48ee42fb0e104cfb885ff0
BryantMonahan <bryantmonahan@gmail.com>
2024-10-23 13:21:14 -0400

    added a message from our beloved group member

--- myFunctions.java
+++ myFunctions.java
@@ -2,4 +2,8 @@ public class myFunctions{
   public void sayGoodbye(){
     System.out.println("Esta noche, yo debo ir mi casa. Hasta manana!");
   }
+
+  public void dropTheClass(){
+    System.out.println("Hey guys, im drunk asf rn and I'm probably going to drop the class but ill still do number 3");
+  }
 }
